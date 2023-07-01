9869498e0fef25b8be18a7222b021fb83198c47a
nicolf <78940021+nicolf@users.noreply.github.com>
2023-07-01 13:11:27 -0300

    Cambios clase 4 - Lista ordenada con flatlist, modales y eliminacion

--- firstRNApp/styles.js
+++ firstRNApp/styles.js
@@ -22,5 +22,71 @@ export const styles = StyleSheet.create({
     color: '#220901',
     height: 40,
     fontSize: 14
+  },
+  listContainer: {
+    marginTop: 25
+  },
+  containerItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 20,
+    backgroundColor: '#D4D7ED',
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    color: '#212121',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+
+    elevation: 3
+  },
+  listItem: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#212121'
+  },
+  list: {
+    gap: 15,
+    paddingBottom: 20
+  },
+  icon: {
+    color: 'red',
+    fontSize: 16
+  },
+  modalContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 60,
+    paddingVertical: 20
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10
+  },
+  modalDetailContainer: {
+    paddingVertical: 20
+  },
+  modalDetailMessage: {
+    fontSize: 14,
+    color: '#212121'
+  },
+  modalSelectedTask: {
+    fontSize: 14,
+    color: '#212121',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingVertical: 10
+  },
+  modalButtonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-around',
+    marginHorizontal: 20
   }
 });
